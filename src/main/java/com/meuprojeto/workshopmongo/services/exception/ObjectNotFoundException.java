7b53f61240e77ccfaa3498cb5207fe631ad29996
Miguel Malton <miguel.malton@gmail.com>
2024-08-05 13:04:17 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/meuprojeto/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.meuprojeto.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
